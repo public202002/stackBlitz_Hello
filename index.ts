67b9cbb80905e14ccefa62bae9fe7011c5d9d079
public202002 <public202002@gmail.com>
2021-01-22 11:07:04 +0100

    exhaustMap

--- index.ts
+++ index.ts
@@ -1,1 +1,8 @@
-console.log(11111111)
+import { fromEvent, interval } from 'rxjs';
+import { exhaustMap, take } from 'rxjs/operators';
+
+const clicks = fromEvent(document, 'click');
+const result = clicks.pipe(
+  exhaustMap(ev => interval(1000).pipe(take(5)))
+);
+result.subscribe(x => console.log(x));
