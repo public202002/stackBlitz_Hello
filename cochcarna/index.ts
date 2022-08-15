c9359571f81b4cdfdfeed37a8c2c4d5a5a24f161
public202002 <public202002@gmail.com>
2022-08-15 16:48:51 +0200

    Cochcara Decision Simple

--- cochcarna/index.ts
+++ cochcarna/index.ts
@@ -1,16 +1,46 @@
-var cochcarna = {createActivity:null};
+var cochcarna = {createActivity:null,
+                 createDecision:null};
+
 cochcarna.createActivity = ()=>{
-  var _activity = {finish:null, task:null,exec:null}
+  var _decision = {finish:null, task:null,exec:null}
   var _finish = (context)=>context;
-  _activity.finish =(next)=>{
+  _decision.finish =(next)=>{
      if(next){_finish=next}
   }
-  _activity.task = (context)=>context;
-  _activity.exec = (context)=>{
-    var retVal = _activity.task(context)
+  _decision.task = (context)=>context;
+  _decision.exec = (context)=>{
+    var retVal = _decision.task(context)
     return _finish(retVal)
   }
-  return _activity
+  return _decision
+}
+
+cochcarna.createDecision = ()=>{
+  var _decision = {finishTrue:null, finishFalse:null, 
+                   task:null,exec:null}
+  var _finishTrue = (context)=>context;
+  var _finishFalse = (context)=>context;
+  _decision.finishTrue =(next)=>{
+    if(next){_finishTrue=next}
+ }
+ _decision.finishFalse =(next)=>{
+  if(next){_finishFalse=next}
+ }
+ _decision.task = (context)=>{return {decision:null, context:context}};
+ _decision.exec = (context)=>{
+    var retVal = _decision.task(context)
+
+    if (retVal.decion===true){
+      return _finishTrue(retVal.context)
+    }
+    if (retVal.decion===false){
+      return _finishTrue(retVal.context)
+    }
+    
+    // Cochcarna Decision Simple
+    throw {error:"Error, wrong decison", description:"see: Cochcarna Decision Simple"}
+ } 
+ return _decision
 }
 
 export default cochcarna
